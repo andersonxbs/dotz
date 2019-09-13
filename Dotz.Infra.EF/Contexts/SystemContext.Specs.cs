972c3d40ef1237e470e75bd5d68a53ab97e7520a
Anderson B. Silva <andersonxbs@gmail.com>
2019-09-13 11:54:33 -0300

    Refatorar configurações de DbContext

---
+++ Dotz.Infra.EF/Contexts/SystemContext.Specs.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+
+namespace Dotz.Infra.EF.Contexts
+{
+    public partial class SystemContext
+    {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            foreach (var entityType in builder.Model.GetEntityTypes())
+            {
+                foreach (var property in entityType.GetProperties())
+                {
+                    if (property.ClrType == typeof(bool))
+                    {
+                        property.SetValueConverter(new BoolToZeroOneConverter<Int16>());
+                    }
+                }
+            }
+        }
+    }
+}
--- Dotz.Infra.EF/Contexts/SystemContext.cs
+++ Dotz.Infra.EF/Contexts/SystemContext.cs
@@ -1,31 +1,13 @@
 ﻿using Dotz.Domain.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using System;
 
 namespace Dotz.Infra.EF.Contexts
 {
-    public class SystemContext : IdentityDbContext<User>
+    public partial class SystemContext : IdentityDbContext<User>
     {
         public SystemContext(DbContextOptions options)
             : base(options)
         { }
-
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-
-            foreach (var entityType in builder.Model.GetEntityTypes())
-            {
-                foreach (var property in entityType.GetProperties())
-                {
-                    if (property.ClrType == typeof(bool))
-                    {
-                        property.SetValueConverter(new BoolToZeroOneConverter<Int16>());
-                    }
-                }
-            }
-        }
     }
 }
